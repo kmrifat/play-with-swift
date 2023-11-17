6ad24e42d8f511df9774d9d1a12929409070aa28
kmrifat <kmrifat@gmail.com>
2023-11-17 19:55:33 +0600

    Initial Commit

---
+++ PlayWithSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayWithSwift
+//
+//  Created by rifat on 17/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayWithSwift/PlayWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayWithSwiftApp.swift
+//  PlayWithSwift
+//
+//  Created by rifat on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
